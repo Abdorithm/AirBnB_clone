fc39ee928a0b9ab4053a6760eb34a9c23f5824e7
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-10-12 15:15:33 +0200

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class BaseModel(cmd.Cmd):
     """Represents BaseModel class"""
-    def __init__(self, id=None, created_at, updated_at):
+    def __init__(self, id=None, created_at=None, updated_at=None):
         """initializes base class
             Args:
                 id (int):
@@ -19,9 +19,20 @@ class BaseModel(cmd.Cmd):
 
     def __str__(self):
         """overriding the __str__ method"""
-        return "[BaseModel] ({}) <{}>".format(self.id, self.__dict__)
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at with
         the current datetime"""
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing
+        all keys/values of __dict__ of the instance"""
+        self.__dict__["__class__"] = "BaseModel"
+        for key in self.__dict__:
+            if key == "updated_at":
+                self.__dict__[key] = self.__dict__[key].isoformat()
+            elif key == "created_at":
+                self.__dict__[key] = self.__dict__[key].isoformat()
+        return self.__dict__
