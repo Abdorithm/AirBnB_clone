ac7dbc9e088c0b450f20f361dd22d3cb3162623e
HazemUsama <hazemosama681@gmail.com>
2023-10-12 23:59:22 +0300

    Checks for pycodestyle and docstring

---
+++ tests/test_style.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import pycodestyle
+
+
+class TestStyle(unittest.TestCase):
+    def test_pycodestyle(self):
+        style = pycodestyle.StyleGuide()
+        project_dir = '.'
+
+        python_files = []
+        for root, _, files in os.walk(project_dir):
+            for file in files:
+                if file.endswith('.py'):
+                    python_files.append(os.path.join(root, file))
+
+        for python_file in python_files:
+            result = style.check_files([python_file])
+            errors = result.total_errors
+
+            self.assertEqual(errors, 0, f"violations in {python_file}")
+
+
+if __name__ == '__main__':
+    unittest.main()
