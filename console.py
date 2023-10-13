21ec332bce3d8de057c73a1ef82a8773e79d8874
HazemUsama <hazemosama681@gmail.com>
2023-10-13 20:49:27 +0300

    do_create

--- console.py
+++ console.py
@@ -2,10 +2,15 @@
 """Defines the HBNBCommand class"""
 import cmd
 import os
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Represent HBNBCommand class"""
     prompt = "(hbnb) "
+    __classes = {
+            "BaseModel"
+    }
     def do_EOF(self, line):
         """Exit the program"""
         return True
@@ -17,6 +22,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing if line is empty"""
         pass
+
+    def do_create(self, line):
+        """Create an instance of Class and prints the id.
+        Usage: Create <class name>"""
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        print(eval(args[0])().id)
+        storage.save()
 
 
 if __name__ == '__main__':
--- tests/test_style.py
+++ tests/test_style.py
@@ -18,9 +18,6 @@ class TestStyle(unittest.TestCase):
             result = style.check_files([python_file])
             errors = result.total_errors
 
-            if errors != 0:
-                print(f"violations in {python_file}: {errors} errors")
-
             self.assertEqual(errors, 0, f"violations in {python_file}")
 
 
