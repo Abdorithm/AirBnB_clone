ffb5052eea8997078315e430bac94e4374da40a5
HazemUsama <hazemosama681@gmail.com>
2023-10-13 21:07:19 +0300

    Added do_all

--- console.py
+++ console.py
@@ -36,6 +36,20 @@ class HBNBCommand(cmd.Cmd):
         print(eval(args[0])().id)
         storage.save()
 
+    def do_all(self, line):
+        """Prints all string representation of all instances.
+        Usage: all <class name>"""
+        args = line.split()
+        if len(args) != 0 and args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        objs = []
+        for obj in storage.all().values():
+            if len(args) == 0:
+                objs.append(obj.__str__())
+            elif obj.__class__.__name__ == args[0]:
+                objs.append(obj.__str__())
+        print (objs)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
