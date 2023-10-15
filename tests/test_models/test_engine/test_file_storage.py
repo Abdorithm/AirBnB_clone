495ef9a02653fd103e986161cb24c7c119b41712
HazemUsama <hazemosama681@gmail.com>
2023-10-15 19:58:54 +0300

    more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -24,6 +24,11 @@ class TestFileStorage_instantiation(unittest.TestCase):
     def test_storage_initializes(self):
         self.assertEqual(type(models.storage), FileStorage)
 
+    def test_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
 class TestFileStorage_methods(unittest.TestCase):
     """Unittest for testing methods of the FileStorage class."""
