50bba4157db2855cde05c80b2acd8f4ea1f94b58
HazemUsama <hazemosama681@gmail.com>
2023-10-15 17:13:35 +0300

    Added More Classes

--- models/__init__.py
+++ models/__init__.py
@@ -3,10 +3,21 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 classes_dict = {
             'BaseModel': BaseModel,
-            'User': User
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
         }
 storage = FileStorage()
 storage.reload()
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents City class"""
+    state_id = ""
+    name = ""
