5738bde3e29632c5aca8d723974069ef6c75f55b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-10-16 00:01:49 +0200

    test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""Test cases for the City from models/city.py
+"""Test cases for the Amenity from models/amenity.py
 
 Unittest classes:
-    TestCity
+    TestAmenity
 """
 import unittest
 from datetime import datetime
-from models.city import City
+from models.amenity import Amenity
 
 
 class TestBaseModel(unittest.TestCase):
-    """Unittest for testing the City class."""
+    """Unittest for testing the Amenity class."""
 
     def setUp(self):
-        self.model = City()
+        self.model = Amenity()
         self.model_dict = self.model.to_dict()
 
     def test_instantiation(self):
-        self.assertIsInstance(self.model, City)
+        self.assertIsInstance(self.model, Amenity)
 
     def test_attr_empty_string(self):
-        self.assertEqual(self.model.state_id, "")
         self.assertEqual(self.model.name, "")
 
 if __name__ == '__main__':
