f54c28ae896d93fc706430f205b7a13c86cfbe1d
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-10-15 23:57:33 +0200

    state and city tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Test cases for the City from models/city.py
+
+Unittest classes:
+    TestCity
+"""
+import unittest
+from datetime import datetime
+from models.city import City
+
+
+class TestBaseModel(unittest.TestCase):
+    """Unittest for testing the City class."""
+
+    def setUp(self):
+        self.model = City()
+        self.model_dict = self.model.to_dict()
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.model, City)
+
+    def test_attr_empty_string(self):
+        self.assertEqual(self.model.state_id, "")
+        self.assertEqual(self.model.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
